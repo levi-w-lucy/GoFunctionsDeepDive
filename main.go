9f18d591ffbd0ad1c9ca006e0e28448ea889fce4
levi-w-lucy <reachfan169@gmail.com>
2024-06-13 17:16:06 -0500

    Using recursion to create a factorial function

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(factorial(-7))
+}
+
+func factorial(number int) int {
+	if number <= 1 {
+		return 1
+	} else {
+		return number * factorial(number-1)
+	}
 
 }
