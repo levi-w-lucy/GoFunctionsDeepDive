16d083855d45b79826d65c3c3a939b7ade859ba4
levi-w-lucy <reachfan169@gmail.com>
2024-06-13 17:07:44 -0500

    Adding code to a new anonymous package for next module

--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	numbers := []int{1, 2, 3}
 
-	transformed := transformNumbers(&numbers, createTransformer(2))
-
-	fmt.Println(transformed)
-}
-
-func transformNumbers(numbers *[]int, transform func(int) int) []int {
-	dNumbers := []int{}
-
-	for _, val := range *numbers {
-		dNumbers = append(dNumbers, transform(val))
-	}
-
-	return dNumbers
-}
-
-func createTransformer(multiplier int) func(int) int {
-	return func(num int) int {
-		return num * multiplier
-	}
 }
