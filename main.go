62d99caefbf0a9423d691670d71ed60593106931
levi-w-lucy <reachfan169@gmail.com>
2024-06-13 08:10:32 -0500

    Demo on using functions as function parameters

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import "fmt"
 
+type transformFunc func(int) int
+
+func main() {
+	numbers := make([]int, 4)
+	for i := 0; i < 4; i++ {
+		numbers[i] = i + 1
+	}
+
+	doubled := transformNumbers(&numbers, double)
+	fmt.Println(doubled)
+
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println(tripled)
+}
+
+func transformNumbers(numbers *[]int, transform transformFunc) []int {
+	dNumbers := []int{}
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+
+	return dNumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
 }
